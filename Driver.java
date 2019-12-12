673f3b98597dde4c7d8e2303a46466d82ee9ccd0
Giovanni-Tedesco <johntedesco@Johns-Air.ycdsb.ca>
2019-12-12 09:10:47 -0500

    Forces the panel to run at 60fps

--- Driver.java
+++ Driver.java
@@ -3,14 +3,21 @@ import javax.swing.*;
 import java.awt.event.*;
 import javax.swing.event.*;
 
-public class Driver {
+public class Driver implements ActionListener {
 
   //Properties
   JFrame frame = new JFrame("Chess");
   BoardAnimation panel = new BoardAnimation();
+  Timer timer = new Timer(1000/60, this);
 
 
   //Methods
+  public void actionPerformed(ActionEvent evt){
+    if(evt.getSource() == timer){
+      panel.repaint();
+    }
+  }
+
 
 
   //Constructor
@@ -23,7 +30,7 @@ public class Driver {
     frame.setContentPane(panel);
     frame.pack();
     frame.setVisible(true);
-
+    timer.start();
 
 
   }
