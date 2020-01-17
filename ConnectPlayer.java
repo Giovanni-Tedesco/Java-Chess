935e2becadb4a59955ddadade72e27fd55e4e9e8
Mehul Pillai <mehulpillai30@gmail.com>
2020-01-16 19:18:50 -0500

    Add timer to connect player

--- ConnectPlayer.java
+++ ConnectPlayer.java
@@ -5,21 +5,25 @@ import javax.swing.event.*;
 
 public class ConnectPlayer implements ActionListener {
 
-	//Properties
+    //Properties
     private JPanel connectPanel = new JPanel(null); //JPanel
+    private Timer connectTimer = new Timer(1000/60, event -> connectPanel.repaint());
     private JButton backButton = new JButton("BACK"); //back button
     private JButton serverButton = new JButton("HOST GAME"); //go to page for host/server computer
-    private JButton clientButton = new JButton("JOIN GAME");//join a game with a server host
+    private JButton clientButton = new JButton("JOIN GAME"); //join a game with a server host
 
-	//Methods
+    //Methods
     @Override
     public void actionPerformed(ActionEvent evt) {
         Object event = evt.getSource(); //set event object as get source method
-        if(event == backButton) {//if back button is clicked
-            Utility.changePanel(new MainMenu().getMenuPanel());//go to main menu
-        } else if(event == serverButton) { //server button
+        if (event == backButton) { //if back button is clicked
+            connectTimer.stop();
+            Utility.changePanel(new MainMenu().getMenuPanel()); //go to main menu
+        } else if (event == serverButton) { //server button
+            connectTimer.stop();
             Utility.changePanel(new ConnectPlayer2(true, false).getConnect2Panel()); //server page
-        } else if(event == clientButton) { //client button
+        } else if (event == clientButton) { //client button
+            connectTimer.stop();
             Utility.changePanel(new ConnectPlayer2(false, false).getConnect2Panel()); //client page
         }
     }
@@ -27,35 +31,35 @@ public class ConnectPlayer implements ActionListener {
         return connectPanel;
     }
 
-	//Constructor
+    //Constructor
     public ConnectPlayer() {
-		//set panel for connecting players
+        //set panel for connecting players
         connectPanel.setBackground(Settings.isDark() ? Color.BLACK : Color.WHITE);
         connectPanel.setPreferredSize(Utility.panelDimensions);
 
-		//set back button
+        //set back button
         backButton.setSize(100, 25);
         backButton.setLocation(30, 20);
         backButton.addActionListener(this);
         Utility.setButtonStyle(backButton, 12); //use preset button style
 
-		//server button
+        //server button
         serverButton.setSize(595, 600);
         serverButton.setLocation(30, 70);
         serverButton.addActionListener(this);
         Utility.setButtonStyle(serverButton, 24);
 
-		//client button
+        //client button
         clientButton.setSize(595, 600);
         clientButton.setLocation(655, 70);
         clientButton.addActionListener(this);
         Utility.setButtonStyle(clientButton, 24);
 
-		//add buttons to panel
+        //add buttons to panel
         connectPanel.add(serverButton);
         connectPanel.add(clientButton);
         connectPanel.add(backButton);
+
+        connectTimer.start();
     }
-
-
 }
